6e8401fcd7a7e0f5ac3a3574f69300d68a9f1788
Drew <tribalxghost@gmail.com>
2023-01-24 19:58:26 -0600

    data

---
+++ index.js
@@ -0,0 +1,43 @@
+// What is HTTP?
+// Hypertext transfer protocal
+// Protocal for transmitting data between web browsers and servers
+
+// What is a URL?
+// Universal resource locator 
+// Address of a given unique resource on the Web.
+
+
+// What is DNS?
+// Domain name system
+// Phonebook of the internet, collection of all IP addresses
+
+// What is a query string?
+// A part of the url that assigns values to specified parameters.
+
+
+// What are two HTTP verbs and how are they different?
+// GET - requests from the server with no changes to server data
+// Post - request for, the server that changes the server data
+
+// What is an HTTP request?
+// referred to as HTTP Verbs to indicate the desired action to be performed for a 
+// given resource
+
+
+// What is an HTTP response?
+// The information the server returns when a request is made.
+
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// HTTP headers let the client and server pass additional info.
+// :authority: www.google.com
+// response: google.com html
+
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+// Makes a protocal to dns/hostname
+// Port 80/the server sends resource
+// page is generated
+
+
+
+
